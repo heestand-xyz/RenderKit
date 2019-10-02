4d0f0e3dbaa550b8eeda78c051e107848da62841
Hexagons <anton.heestand@gmail.com>
2019-10-02 16:24:18 +0200

    Initial Commit

---
+++ RenderKit/RenderKit.h
@@ -0,0 +1,19 @@
+//
+//  RenderKit.h
+//  RenderKit
+//
+//  Created by Anton Heestand on 2019-10-02.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RenderKit.
+FOUNDATION_EXPORT double RenderKitVersionNumber;
+
+//! Project version string for RenderKit.
+FOUNDATION_EXPORT const unsigned char RenderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RenderKit/PublicHeader.h>
+
+
