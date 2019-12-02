7ddc71e404dd8c4a87a41f9855789bdcf8135f13
Hexagons <anton.heestand@gmail.com>
2019-12-02 17:33:52 +0100

    depth camera

--- Source/Texture.swift
+++ Source/Texture.swift
@@ -10,6 +10,7 @@ import CoreGraphics
 import LiveValues
 import MetalKit
 import VideoToolbox
+import Accelerate
 
 public struct Texture {
     
@@ -211,8 +212,8 @@ public struct Texture {
         let channelCount = bytesPerRow / width
         let pixelFormat: MTLPixelFormat
         switch channelCount {
-//        case 4: pixelFormat = bits.pixelFormat
-//        case 2: pixelFormat = LiveColor.Bits(rawValue: bits.rawValue * 2)!.monochromePixelFormat
+        case 4: pixelFormat = bits.pixelFormat
+        case 2: pixelFormat = LiveColor.Bits._16.monochromePixelFormat
         default: pixelFormat = bits.pixelFormat
         }
         
@@ -817,6 +818,73 @@ public struct Texture {
             throw TextureError.raw("rawNormalizedCopy3d with \(bits.rawValue)bits is not supported.")
         }
         return raw
+    }
+    
+    // MARK: - Rotate
+    
+    public static func rotate90(pixelBuffer srcPixelBuffer: CVPixelBuffer, factor: UInt8) -> CVPixelBuffer? {
+      let flags = CVPixelBufferLockFlags(rawValue: 0)
+      guard kCVReturnSuccess == CVPixelBufferLockBaseAddress(srcPixelBuffer, flags) else {
+        return nil
+      }
+      defer { CVPixelBufferUnlockBaseAddress(srcPixelBuffer, flags) }
+
+      guard let srcData = CVPixelBufferGetBaseAddress(srcPixelBuffer) else {
+        print("Error: could not get pixel buffer base address")
+        return nil
+      }
+      let sourceWidth = CVPixelBufferGetWidth(srcPixelBuffer)
+      let sourceHeight = CVPixelBufferGetHeight(srcPixelBuffer)
+      var destWidth = sourceHeight
+      var destHeight = sourceWidth
+      var color = UInt8(0)
+
+      if factor % 2 == 0 {
+        destWidth = sourceWidth
+        destHeight = sourceHeight
+      }
+
+      let srcBytesPerRow = CVPixelBufferGetBytesPerRow(srcPixelBuffer)
+      var srcBuffer = vImage_Buffer(data: srcData,
+                                    height: vImagePixelCount(sourceHeight),
+                                    width: vImagePixelCount(sourceWidth),
+                                    rowBytes: srcBytesPerRow)
+
+      let destBytesPerRow = destWidth*4
+      guard let destData = malloc(destHeight*destBytesPerRow) else {
+        print("rotate90 Error: out of memory")
+        return nil
+      }
+      var destBuffer = vImage_Buffer(data: destData,
+                                     height: vImagePixelCount(destHeight),
+                                     width: vImagePixelCount(destWidth),
+                                     rowBytes: destBytesPerRow)
+
+      let error = vImageRotate90_ARGB8888(&srcBuffer, &destBuffer, factor, &color, vImage_Flags(0))
+      if error != kvImageNoError {
+        print("rotate90 Error:", error)
+        free(destData)
+        return nil
+      }
+
+      let releaseCallback: CVPixelBufferReleaseBytesCallback = { _, ptr in
+        if let ptr = ptr {
+          free(UnsafeMutableRawPointer(mutating: ptr))
+        }
+      }
+
+      let pixelFormat = CVPixelBufferGetPixelFormatType(srcPixelBuffer)
+      var dstPixelBuffer: CVPixelBuffer?
+      let status = CVPixelBufferCreateWithBytes(nil, destWidth, destHeight,
+                                                pixelFormat, destData,
+                                                destBytesPerRow, releaseCallback,
+                                                nil, nil, &dstPixelBuffer)
+      if status != kCVReturnSuccess {
+        print("rotate90 Error: could not create new pixel buffer")
+        free(destData)
+        return nil
+      }
+      return dstPixelBuffer
     }
     
 }
