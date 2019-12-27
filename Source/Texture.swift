03a93a6751793906d0cb4e52f1520bf74e243694
Hexagons <anton@hexagons.se>
2019-12-27 15:16:22 +0100

    image flip fix

--- Source/Texture.swift
+++ Source/Texture.swift
@@ -461,7 +461,7 @@ public struct Texture {
 
     public static func image(from cgImage: CGImage, at size: CGSize) -> _Image {
         #if os(iOS) || os(tvOS)
-        return UIImage(cgImage: cgImage)//, scale: 1, orientation: .downMirrored)
+        return UIImage(cgImage: cgImage, scale: 1, orientation: .downMirrored)
         #elseif os(macOS)
         return NSImage(cgImage: cgImage, size: size)
         #endif
