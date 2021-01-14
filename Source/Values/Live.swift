be87365f560f6a235ab77b5f632afcaae3115f40
Anton Heestand <anton.heestand@gmail.com>
2021-01-14 19:01:25 +0100

    Cleanup

--- Source/Values/Live.swift
+++ Source/Values/Live.swift
@@ -2,11 +2,6 @@ import Foundation
 
 public class LiveProp {
     public var node: NODE!
-//    var getFloatable: () -> Floatable
-//    public var floatable: Floatable { getFloatable() }
-//    internal init(get getFloatable: @escaping () -> Floatable) {
-//        self.getFloatable = getFloatable
-//    }
 }
 
 @propertyWrapper public class Live<F: Floatable>: LiveProp {
@@ -23,26 +18,6 @@ public class LiveProp {
     
     public init(wrappedValue: F) {
         self.wrappedValue = wrappedValue
-//        super.init(get: { wrappedValue })
     }
 
 }
-
-//@propertyWrapper public class LiveArray<F: Floatable>: LiveProp {
-//
-//    public var wrappedValue: [F] {
-//        didSet {
-//            guard let node: NODE = node else {
-//                print("RenderKit Live property wrapper not linked to node.")
-//                return
-//            }
-//            node.setNeedsRender()
-//        }
-//    }
-//
-//    public init(wrappedValue: [F]) {
-//        self.wrappedValue = wrappedValue
-//        super.init(get: { wrappedValue })
-//    }
-//
-//}
