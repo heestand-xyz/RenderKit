9d8ce946b8386e6007b8e63dbb2824a7aa257f5c
Anton Heestand <anton.heestand@gmail.com>
2021-04-21 21:00:34 +0200

    Pixels

--- Source/Values/Live.swift
+++ Source/Values/Live.swift
@@ -273,10 +273,22 @@ public class LiveEnumWrap: LiveWrap {
     public var dynamicTypeNames: [String] {
         names.map { $0.lowercased().replacingOccurrences(of: " ", with: "-") }
     }
+    public var defaultCaseDynamicTypeName: String {
+        let rawIndex: Int = defaultValue as! Int
+        guard let index: Int = rawIndices.firstIndex(of: rawIndex) else { return "" }
+        guard index < dynamicTypeNames.count else { return "" }
+        return dynamicTypeNames[index]
+    }
     public init(name: String, rawIndex: Int, rawIndices: [Int], names: [String]) {
         self.rawIndices = rawIndices
         self.names = names
         super.init(type: .enumable, name: name, value: rawIndex)
+    }
+    public func setCase(typeName: String) {
+        guard let index: Int = dynamicTypeNames.firstIndex(where: { $0 == typeName }) else { return }
+        guard index < rawIndices.count else { return }
+        let rawIndex: Int = rawIndices[index]
+        set(rawIndex)
     }
 }
 
