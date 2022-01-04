2c144804cc30e486190aa82d67291fa068e7ce27
Anton Heestand <anton.heestand@gmail.com>
2022-01-04 12:36:30 +0100

    Nodes

--- Source/Models/NodeReference.swift
+++ Source/Models/NodeReference.swift
@@ -9,21 +9,51 @@ import Foundation
 
 public struct NodeReference: Codable {
     
-    let id: UUID
+    public let id: UUID
+    public let typeName: String
+    public let name: String
     
-    let typeName: String
-    let name: String
-    
-    enum Connection: Codable {
-        case inputSingle
-        enum Merger: Codable {
+    public enum Connection: Codable {
+        case single
+        public enum Merger: Codable {
             case leading
             case trailing
         }
-        case inputMerger(Merger)
-        case inputMulti(Int)
-        case output
+        case merger(Merger)
+        case multi(Int)
     }
-    let connection: Connection
+    public let connection: Connection
+    
+    public init(nodeOutPath: NODEOutPath) {
+        id = nodeOutPath.nodeIn.id
+        typeName = nodeOutPath.nodeIn.typeName
+        name = nodeOutPath.nodeIn.name
+        switch nodeOutPath.nodeIn {
+        case is NODESingleEffect:
+            connection = .single
+        case is NODEMergerEffect:
+            connection = .merger(nodeOutPath.inIndex == 0 ? .leading : .trailing)
+        case is NODEMultiEffect:
+            connection = .multi(nodeOutPath.inIndex)
+        case is NODEOutput:
+            connection = .single
+        default:
+            fatalError("Unknown NODE Type")
+        }
+    }
+    
+    public init(node: NODE, connection: NodeReference.Connection) {
+        self.id = node.id
+        self.typeName = node.typeName
+        self.name = node.name
+        self.connection = connection
+    }
+    
+    public init(id: UUID, typeName: String, name: String, connection: NodeReference.Connection) {
+        self.id = id
+        self.typeName = typeName
+        self.name = name
+        self.connection = connection
+    }
     
 }
